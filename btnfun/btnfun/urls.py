d5277f2e3dd600bd792fef4d1fd80a2acd03bc33
Darcy Liu <darcy.ldx@gmail.com>
2023-12-23 12:01:32 -0500

    made incbtn app and performed migrations

--- btnfun/btnfun/urls.py
+++ btnfun/btnfun/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import  views
+from incbtn import views as incbtnviews
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
-    path('increment/', views.increment, name='increment'),
+    path('', incbtnviews.index, name='index'),
+    path('increment/', incbtnviews.increment, name='increment'),
 ]
